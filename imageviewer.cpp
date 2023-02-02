d1ff41788c3f06da01471ce653b0ef7d04857011
rohezal <rashid@milacom.de>
2023-02-02 12:54:02 +0100

    fixed image viewer code, not included yet

--- imageviewer.cpp
+++ imageviewer.cpp
@@ -1,117 +1,122 @@
-//#include "imageviewer.h"
+#include "imageviewer.h"
 
-//ImageViewer::ImageViewer(QWidget *parent)
-//   : QWidget(parent), ui(UI::ImageViewer)
-//{
-//    ui->setupUI(this);
+ImageViewer::ImageViewer(QWidget *parent)
+   : QWidget(parent), ui(new Ui::ImageViewer)
+{
+	ui->setupUi(this);
 
-//    ui->scrollArea->setBackgroundRole(QPalette::Dark);
+	ui->scrollArea->setBackgroundRole(QPalette::Dark);
 
-//    // set the scrollArea as imageLabel
-//    ui->imageLabel->setParent(this);
-//    delete ui->scrollArea->takeWidget();
-//    ui->scrollAreaWidgetContents = nullptr;
-//    ui->scrollArea->setWidget(ui->imageLabel);
+	/*
 
-//    // actions to the buttons
-//    connect(ui->zoomInButton, SIGNAL(pressed()), SLOT(zoomIn()));
-//    connect(ui->zoomOutButton, SIGNAL(pressed()), SLOT(zoomOut()));
-//    connect(ui->fitToWindowButton, SIGNAL(pressed()), SLOT(fitToWindowButton()));
-//}
+	// set the scrollArea as imageLabel
+	ui->imageLabel->setParent(this);
+	delete ui->scrollArea->takeWidget();
+	ui->scrollAreaWidgetContents = nullptr;
+	ui->scrollArea->setWidget(ui->imageLabel);
 
-//ImageViewer::~ImageViewer()
-//{
-//    delete ui;
-//}
+	// actions to the buttons
+	connect(ui->zoomInButton, SIGNAL(pressed()), SLOT(zoomIn()));
+	connect(ui->zoomOutButton, SIGNAL(pressed()), SLOT(zoomOut()));
+	connect(ui->fitToWindowButton, SIGNAL(pressed()), SLOT(fitToWindowButton()));
+	*/
+}
 
 
-//void ImageViewer::setFilePath(const QString& filePath)
-//{
-//    t_filePath = filePath;
-//}
+ImageViewer::~ImageViewer()
+{
+	delete ui;
+}
 
-//bool ImageViewer::loadImage()
-//{
-//    if(t_filePath.isEmpty())
-//    {
-//        return false;
-//    }
 
-//    if(t_image.load(t_filePath))
-//    {
-//        t_scaleFactor = 1.0;
-//        setScaledImage();
-//        return true;
-//    }
-//    return false;
-//}
+void ImageViewer::setFilePath(const QString& filePath)
+{
+	t_filePath = filePath;
+}
 
-//void ImageViewer::setScaledImage()
-//{
-//    QPixmap scaledPixmap = scaledImage;
-//    if(!scaledPixmap.isNull())
-//    {
-//        ui->imageLabel->setFixedSize(scaledPixmap.size());
-//        ui->imageLabel->setPixmap(scaledPixmap);
-//    }
-//}
+bool ImageViewer::loadImage()
+{
+	if(t_filePath.isEmpty())
+	{
+		return false;
+	}
 
-//QPixmap ImageViewer::scaledImage()
-//{
-//    QSize scaledSize = getScaledSize();
-//    QPixmap scaledPixmap;
-//    if(!t_image.isNull()) {
-//        scaledPixmap = t_image.scaled(scaledSize, QT::KeepAspectRatio, QT::SmoothTransformation);
-//    }
-//    return scaledPixmap;
-//}
+	if(t_image.load(t_filePath))
+	{
+		t_scaleFactor = 1.0;
+		setScaledImage();
+		return true;
+	}
+	return false;
+}
 
-//QSize ImageViewer::getScaledSize()
-//{
-//    QSize scaledSize = size();
-//    scaledSize.setHeight(scaledSize.height() - ui->toolbarFrame->height());
-//    scaledSize *= t_scaleFactor;
-//    scaledSize.setWidth(scakedSize.width() - 5);
-//    scaledSize.setHeight(scaledSize.height() - 5);
-//    return scaledSize;
-//}
+void ImageViewer::setScaledImage()
+{
+	QPixmap scaledPixmap = this->scaledImage();
+	if(!scaledPixmap.isNull())
+	{
+		ui->imageLabel->setFixedSize(scaledPixmap.size());
+		ui->imageLabel->setPixmap(scaledPixmap);
+	}
+}
 
-//void ImageViewer::resizeEvent(QResizeEvent *event)
-//{
-//    QWidget::resizeEvent(event);
-//    setScaledImage();
-//}
 
-//void ImageViewer::wheelEvent(QWheelEvent *event)
-//{
-//    int delta = event->delta();
-//    if(QApplication::keyboardModifiers() & QT::ControlModifier)
-//    {
-//        if(delta > 0)
-//        {
-//            zoomIn();
-//        }
-//        else
-//        {
-//            zoomOut();
-//        }
-//    }
-//}
+QPixmap ImageViewer::scaledImage()
+{
+	QSize scaledSize = getScaledSize();
+	QPixmap scaledPixmap;
+	if(!t_image.isNull()) {
+		scaledPixmap = t_image.scaled(scaledSize, Qt::KeepAspectRatio, Qt::SmoothTransformation);
+	}
+	return scaledPixmap;
+}
 
-//void ImageViewer::zoomIn()
-//{
-//    t_scaleFactor += 0.3;
-//    setScaledImage();
-//}
+QSize ImageViewer::getScaledSize()
+{
+	QSize scaledSize = size();
+	scaledSize *= t_scaleFactor;
+	scaledSize.setWidth(scaledSize.width() - 5);
+	scaledSize.setHeight(scaledSize.height() - 5);
+	return scaledSize;
+}
 
-//void ImageViewer::zoomOut()
-//{
-//    t_scaleFactor -= 0.3;
-//    setScaledImage();
-//}
 
-//void ImageViewer::fitToWindow()
-//{
-//    t_scaleFactor = 1.0;
-//    setScaledImage();
-//}
+void ImageViewer::resizeEvent(QResizeEvent *event)
+{
+	QWidget::resizeEvent(event);
+	setScaledImage();
+}
+
+void ImageViewer::wheelEvent(QWheelEvent *event)
+{
+	int delta = event->delta();
+	if(QApplication::keyboardModifiers() & Qt::ControlModifier)
+	{
+		if(delta > 0)
+		{
+			zoomIn();
+		}
+		else
+		{
+			zoomOut();
+		}
+	}
+}
+
+void ImageViewer::zoomIn()
+{
+	t_scaleFactor += 0.3;
+	setScaledImage();
+}
+
+void ImageViewer::zoomOut()
+{
+	t_scaleFactor -= 0.3;
+	setScaledImage();
+}
+
+void ImageViewer::fitToWindow()
+{
+	t_scaleFactor = 1.0;
+	setScaledImage();
+}
--- imageviewer.h
+++ imageviewer.h
@@ -1,46 +1,46 @@
-//#ifndef IMAGEVIEWER_H
-//#define IMAGEVIEWER_H
+#ifndef IMAGEVIEWER_H
+#define IMAGEVIEWER_H
 
-//#include <QWidget>
+#include <QWidget>
+#include <QWheelEvent>
+#include "ui_imageviewer.h"
 
-//namespace Ui {
-//class ImageViewer;
-//}
+class ImageViewer : public QWidget
+{
+	Q_OBJECT
 
-//class ImageViewer : public QWidget
-//{
-//    Q_OBJECT
+public:
+	explicit ImageViewer(QWidget *parent = 0);
+	~ImageViewer();
 
-//public:
-//    explicit ImageViewer(QWidget *parent = 0);
 
-//    // functions to load the image into the ImageViewer
-//    bool loadImage();
-//    void setFilePath(const QString& filepath);
+	// functions to load the image into the ImageViewer
+	bool loadImage();
+	void setFilePath(const QString& filepath);
 
-//private slots:
-//    // buttonActions for zooming
-//    void zoomIn();
-//    void zoomOut();
-//    void fitToWindow();
+private slots:
+	// buttonActions for zooming
+	void zoomIn();
+	void zoomOut();
+	void fitToWindow();
 
-//protected:
-//    // events for resizing window or using the mouswheel
-//    void resizeEvent(QResizeEvent *event) override;
-//    void wheelEvent(QWheelEvent *event) override;
+protected:
+	// events for resizing window or using the mouswheel
+	void resizeEvent(QResizeEvent *event) override;
+	void wheelEvent(QWheelEvent *event) override;
 
-//private:
-//    // functions to scale the image
-//    QPixmap scaledImage();
-//    void setScaledImage();
-//    QSize getScaledSize();
+private:
+	// functions to scale the image
+	QPixmap scaledImage();
+	void setScaledImage();
+	QSize getScaledSize();
 
-//    // variables for the image
-//    UI::ImageViewer *ui;
-//    QPixmap t_image;
-//    double t_scaleFactor = 1.0;
-//    QString t_filePath;
+	// variables for the image
+	Ui::ImageViewer *ui;
+	QPixmap t_image;
+	double t_scaleFactor = 1.0;
+	QString t_filePath;
 
-//};
+};
 
-//#endif // IMAGEVIEWER_H
+#endif // IMAGEVIEWER_H
